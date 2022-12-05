c8eb0e3f583e04461bc7f0d7e991999eb658037b
Davi Noronha <davinoronhaalmeida@gmail.com>
2022-12-05 19:35:05 -0300

    context

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -11,6 +11,21 @@ export default function Formulario({ children, textSubmit }) {
         const body = Object.fromEntries(formData.entries());
         setModalOpened(true);
         console.log(body);
+        try {
+            const data = await fetch("http://localhost:8000/login", {
+                mode: "no-cors",
+                method: "POST",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                },
+                body: body,
+            });
+            console.log(data);
+            const info = React.createContext(body);
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     return (
